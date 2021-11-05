559b2ed03d27e471afc867b53ccf31975e2eb66e
hilalsidhic <hilalsidhic21@gmail.com>
2021-11-05 22:58:10 +0530

    stateful completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,111 +14,152 @@ class Myapp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.indigo,
       ),
-      home: HomePage(),
+      home: newHomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+class newHomePage extends StatefulWidget {
+   newHomePage({ Key? key }) : super(key: key);
+
+  @override
+  State<newHomePage> createState() => _newHomePageState();
+}
+
+class _newHomePageState extends State<newHomePage> {
+    final  _textController= TextEditingController();
+    var _newText='This is were the text appears';
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: Colors.grey,
-      // appBar: AppBar(),
+    return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              flex: 2,
-              child: 
-            Container(
-         color: Colors.blue,
-          child: Center(
-            child:  Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-               children: [
-              const  Text(
-              'Hilal',           
-                style: TextStyle(
-                color: Colors.purple,
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            color: Colors.amber,
+            child: Column(
+              mainAxisAlignment:MainAxisAlignment.center,
               children: [
-              TextButton(onPressed: (){
-              print("Button clicked");
-            }, child: const Text("Click Me")),
-              IconButton(onPressed: (){
-                print("icon button pressed");
-              }, icon: Icon(Icons.mic)),
-            ],),
-            ElevatedButton(onPressed: (){
-              print(" new Button clicked");
-            }, child: const Text("Click Me")),
+                TextField(
+                  controller: _textController,
+                  decoration: InputDecoration(border: OutlineInputBorder(),hintText: "Type Something"),
+                  
+                ),
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    _newText=_textController.text;
+                  });
+                  
+                }, child: Text("Click Me")),
+                Text(_newText),
               ],
-            )
+            ),
           ),
-        ),
-            ),
-        Expanded(
-          flex: 1,
-          child:  Container(
-            decoration:  BoxDecoration(
-              color: Colors.amber,
-              borderRadius:BorderRadius.circular(40),
-              border: Border.all(
-                color: Colors.green,
-                width: 10,
-              )
-            ),
-            child: Center(child: const Text("New text")),
-          )),
-        Expanded(
-          flex:2,
-          child: 
-        Container(
-          color: Colors.deepPurple,
-          child: Center(
-            child:  Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-               children: [
-              const  Text(
-              'Hilal',           
-                style: TextStyle(
-                color: Colors.purple,
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              TextButton(onPressed: (){
-              print("Button clicked");
-            }, child: const Text("Click Me")),
-              IconButton(onPressed: (){
-                print("icon button pressed");
-              }, icon: Icon(Icons.mic)),
-            ],),
-            ElevatedButton(onPressed: (){
-              print(" new Button clicked");
-            }, child: const Text("Click Me")),
-              ],
-            )
-          ),
-        )
-        ),
-          ],
-        )
-      ),
+        )),
     );
   }
 }
+// class HomePage extends StatelessWidget {
+//   const HomePage({ Key? key }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return  Scaffold(
+//       backgroundColor: Colors.grey,
+//       // appBar: AppBar(),
+//       body: SafeArea(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: [
+//             Expanded(
+//               flex: 2,
+//               child: 
+//             Container(
+//          color: Colors.blue,
+//           child: Center(
+//             child:  Column(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//                children: [
+//               const  Text(
+//               'Hilal',           
+//                 style: TextStyle(
+//                 color: Colors.purple,
+//                 fontSize: 40,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//               TextButton(onPressed: (){
+//               print("Button clicked");
+//             }, child: const Text("Click Me")),
+//               IconButton(onPressed: (){
+//                 print("icon button pressed");
+//               }, icon: Icon(Icons.mic)),
+//             ],),
+//             ElevatedButton(onPressed: (){
+//               print(" new Button clicked");
+//             }, child: const Text("Click Me")),
+//               ],
+//             )
+//           ),
+//         ),
+//             ),
+//         Expanded(
+//           flex: 1,
+//           child:  Container(
+//             decoration:  BoxDecoration(
+//               color: Colors.amber,
+//               borderRadius:BorderRadius.circular(40),
+//               border: Border.all(
+//                 color: Colors.green,
+//                 width: 10,
+//               )
+//             ),
+//             child: Center(child: const Text("New text")),
+//           )),
+//         Expanded(
+//           flex:2,
+//           child: 
+//         Container(
+//           color: Colors.deepPurple,
+//           child: Center(
+//             child:  Column(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//                children: [
+//               const  Text(
+//               'Hilal',           
+//                 style: TextStyle(
+//                 color: Colors.purple,
+//                 fontSize: 40,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//               TextButton(onPressed: (){
+//               print("Button clicked");
+//             }, child: const Text("Click Me")),
+//               IconButton(onPressed: (){
+//                 print("icon button pressed");
+//               }, icon: Icon(Icons.mic)),
+//             ],),
+//             ElevatedButton(onPressed: (){
+//               print(" new Button clicked");
+//             }, child: const Text("Click Me")),
+//               ],
+//             )
+//           ),
+//         )
+//         ),
+//           ],
+//         )
+//       ),
+//     );
+//   }
+// }
